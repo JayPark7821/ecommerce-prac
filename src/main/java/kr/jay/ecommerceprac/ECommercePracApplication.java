d1d337066f01e5097d05d0a406c47ab268b6849a
jaypark7821 <pjhyun1990@gmail.com>
2023-01-18 23:26:49 +0900

    initial commit

---
+++ src/main/java/kr/jay/ecommerceprac/ECommercePracApplication.java
@@ -0,0 +1,13 @@
+package kr.jay.ecommerceprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercePracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/jay/ecommerceprac/ECommercePracApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.jay.ecommerceprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
